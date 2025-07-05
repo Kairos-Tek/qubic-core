66ad8e1c5a1da1197876c1d03a0a8d31a9700240
Jorge Ordovás <jorge.ordovas@gmail.com>
2025-07-05 16:57:03 +0200

    Update HM25.h

--- src/contracts/HM25.h
+++ src/contracts/HM25.h
@@ -7,11 +7,15 @@ struct HM252
 struct HM25 : public ContractBase
 {
 public:
-    struct _input{};
+    struct proc01_input{};
     struct proc01_output{};
+    struct proc02_input{};
     struct proc02_output{};
     struct proc03_output{};
+    struct proc03_input{};
+    struct proc04_input{};
     struct proc04_output{};
+    struct proc05_input{};
     struct proc05_output{};
 
     struct func01_input{};
@@ -22,9 +26,29 @@ public:
     
 private:
     uint64 var1;
+    uint64 var2;
+    uint64 var3;
+    uint64 var4;
+    uint64 var5;
 
     PUBLIC_PROCEDURE(proc01)
         state.var1++;
+   _
+
+    PUBLIC_PROCEDURE(proc02)
+        state.var2++;
+   _
+
+    PUBLIC_PROCEDURE(proc03)
+        state.var3++;
+   _
+
+    PUBLIC_PROCEDURE(proc04)
+        state.var4++;
+   _
+
+    PUBLIC_PROCEDURE(proc05)
+        state.var5++;
    _
 
     PUBLIC_FUNCTION(func01)
@@ -34,11 +58,19 @@ private:
     REGISTER_USER_FUNCTIONS_AND_PROCEDURES
 
         REGISTER_USER_PROCEDURE(proc01, 1);
+        REGISTER_USER_PROCEDURE(proc02, 2);
+        REGISTER_USER_PROCEDURE(proc03, 3);
+        REGISTER_USER_PROCEDURE(proc04, 4);
+        REGISTER_USER_PROCEDURE(proc05, 5);
 
         REGISTER_USER_FUNCTION(func01, 1);
     _
 
     INITIALIZE
         state.var1 = 0;
+        state.var2 = 0;
+        state.var3 = 0;
+        state.var4 = 0;
+        state.var5 = 0;
     _
 };
